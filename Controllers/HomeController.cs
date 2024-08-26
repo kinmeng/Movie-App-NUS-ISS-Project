9c11221b5551f0b7b534cac73af13e45f833c3b9
Joel Wee Wei Jie <d1356480@ncsdevnet.com>
2024-08-26 20:24:03 +0800

    [New] Simple about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace MovieApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
